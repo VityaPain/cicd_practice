f90c71ab58a58da4facfc2a9153ad0f2569de2e5
Victor Zablockii <vitayzablockyy58@gmail.com>
2025-06-09 14:23:51 +0300

    рефактор

--- src/components/screens/home/Details.jsx
+++ src/components/screens/home/Details.jsx
@@ -2,19 +2,7 @@ import React from 'react'
 
 const Details = () => {
 	return (
-		<div className='absolute right-1 bottom-0 flex items-center text-white font-bold text-lg'>
-			<div
-				className='pr-2'
-				style={{ borderRight: '2px solid rgba(255, 255, 255, .3)' }}
-			>
-				2025
-			</div>
-			<div className='px-2'>
-				<span className='bg-white text-black rounded-sm px-2 py-1 mr-2'>
-					КиноПоиск
-				</span>
-			</div>
-		</div>
+		<div className='absolute right-1 bottom-0 flex items-center text-white font-bold text-lg'></div>
 	)
 }
 
